0c6dfc07ae118b9e9c92e31444546ae4ffae438e
Emily Ahn <eahn@wellesley.edu>
2015-11-04 15:27:06 -0500

    use vstack, read args from commandline, testing done for n_comp= 1, 2

--- gmmClassifier.py
+++ gmmClassifier.py
@@ -12,25 +12,22 @@ from sklearn.mixture import GMM
 import os
 import sys
 
-def train_model(speech_array_list, n_components): #, covar_type
+def train_model(speech_array_list, n_components, covar_type): 
     """train GMM with n_components mixtures from data,
     represented as a list of arrays, where each array contains the
     PLPs from a speech file. Returns this model."""
     # to do
     # experiment w/ diff covariance types (ex. 'spherical'), n_components
-    # covar types: ['spherical', 'diag', 'tied', 'full']
-    # covar --> parameter
     
     # WARNING: takes long time!!
-    bigArray = []
-    for single_array in speech_array_list:
+    #bigArray = []
+    '''for single_array in speech_array_list:
         for one_row in single_array:
-            bigArray.append(one_row)
-    #bigArray = np.array(speech_array_list).flatten()
-    #print "first 2 lines of bigArray: ", bigArray[0], " +++++ ", bigArray[1]
+            bigArray.append(one_row)'''
+    bigArray = np.vstack(speech_array_list)
     print "bigArray DONE"
-    g = GMM(n_components=n_components,covariance_type='tied', init_params='wc', n_iter=20)
-    g.fit(np.array(bigArray))
+    g = GMM(n_components=n_components,covariance_type=covar_type, init_params='wc', n_iter=20)
+    g.fit(bigArray)
     print "fitting DONE"
     return g
 
@@ -43,9 +40,9 @@ def apply_model(gmm_model, speech_array):
 
 if __name__=='__main__':
     """load .npz data, split into train-test folds, run training and testing"""
-    npzdir = 'cslu_fae_corpus/npz'#sys.argv[1] # # directory with npz files
-    n_components = 5 #sys.argv[2]  # number of GMM components
-    #covar = sys.argv[3]
+    npzdir = sys.argv[1] #'cslu_fae_corpus/npz' # directory with npz files
+    n_components = int(sys.argv[2])  # number of GMM components
+    covar = sys.argv[3]    # covar types: ['spherical', 'diag', 'tied', 'full']
 
     langlist = ['AR', 'MA', 'HI']
     n_folds = 4
@@ -64,6 +61,8 @@ if __name__=='__main__':
         labels.extend([li]*len(data[lang]))
     
     folds = StratifiedKFold(labels, n_folds = n_folds, shuffle = True)
+    # initialize list to put accuracy of each fold, to be averaged over & printed
+    accuracy_list = np.empty(len(folds))
     for foldid, (train_indices, test_indices) in enumerate(folds):
         models = {}   # store models for each lang
         for li, lang in enumerate(langlist):
@@ -75,7 +74,7 @@ if __name__=='__main__':
                                    for i in train_lang_indices]
             # list of PLP arrays corresponding to filenames in training
                         
-            models[lang] = train_model(train_lang_list, n_components) #, covar
+            models[lang] = train_model(train_lang_list, n_components, covar)
         
         # now test
         accuracy = 0
@@ -89,5 +88,7 @@ if __name__=='__main__':
             predicted_lang = max(logprobs.items(), key=lambda x:x[1])[0]
             if actual_lang == predicted_lang:
                 accuracy += 1
-        
-        print 'Accuracy for fold', foldid, 'is', accuracy/len(test_indices)
+        accuracy_list[foldid] = accuracy/len(test_indices)
+        print 'Accuracy for fold', foldid, 'is', accuracy_list[foldid]
+
+    print 'AVG over', len(folds), 'folds is', np.average(accuracy_list)
