9dcfe2696ecb3e29b60a7d663b61176c48417675
Emily Ahn <eahn@wellesley.edu>
2015-11-18 23:56:41 -0500

    updated joinAudio file

--- joinAudio.py
+++ joinAudio.py
@@ -1,14 +1,16 @@
 # concatenate filenames (with delimeter file) into a string
 # use string to enter sox command into terminal
 
+
 from os import listdir
 
 dirNames = ['cslu_fae_corpus/speech/AR', 'cslu_fae_corpus/speech/CZ', 'cslu_fae_corpus/speech/IN']
 #files_AR = listdir(dirNames[0])
 #files_CZ= listdir(dirNames[1])
-files_IN = listdir(dirNames[2])
+#files_IN = listdir(dirNames[2])
 
 single = ""
-for filename in files_IN:
-    single += filename + " delim-bird.wav "
+# enter index # to change name
+for filename in listdir(dirNames[2]):
+    single += filename + " marwick.wav "
 print "sox " + single
