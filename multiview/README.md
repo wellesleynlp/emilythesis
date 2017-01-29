16048e9e12c8d21308763dd1a49b074f57b2cb82
Emily Ahn <eahn@wellesley.edu>
2017-01-29 00:00:44 -0800

    logistic regression. accuracy 61%

--- multiview/README.md
+++ multiview/README.md
@@ -1,4 +1,26 @@
 ## 1. TASK: Do Accent ID only using text features
+1/29
+- used [logistic regression](http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html) from sklearn 
+Overall Accuracy: *_61.18%_*
+| lang | acc | acc/total\_acc / rel # test files
+----|-----|------------|
+|AR | 32.00% | 1.07 |
+|CZ | 21.74% | _0.79_ |
+|FR | 75.81% | 1.03 |
+|HI | 77.22% | 0.82 |
+|IN | 38.10% | _1.52_ |
+|KO | 50.00% | 1.17 |
+|MA | 67.24% | 0.97 |
+
+- general analysis of test set: # of test files and % out of whole test set
+	AR: 25 (8.2%)
+	CZ: 23 (7.6%)
+	FR: 62 (20.4%)
+	HI: 79 (26.0%)
+	IN: 21 (6.9%)
+	KO: 36 (11.8%)
+	MA: 58 (19.1%)
+
 1/22
 - for 7 langs, # of 20sec files maintained (out of total files): 
 	- 922/1045 (88%) in train: `/traintestsplit/7lang.trainlist.sorted.20sec`
@@ -28,7 +50,7 @@ Stored predictions in `multiview/knn_predictions_170123/knn.{k}.predictions` for
 | 30 | 39.47% |
 | 40 | 40.79% |
 
-For future:
-- logistic regression - http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+-----------------------
+MISC:
 - k neighbors regressor (?) - http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsRegressor.html#sklearn.neighbors.KNeighborsRegressor
 - csr\_matrix - https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.sparse.csr_matrix.html
--- multiview/baseline_wordvec_cluster.py
+++ multiview/baseline_wordvec_cluster.py
@@ -1,9 +1,10 @@
 """ Using only unigram vectors from transcriptions of accented speech, 
     try to cluster and do accent ID. Clustering includes: 
         - knn (method from knncluster.py in A5 from NLP class)
+        - logistic regression (module from sklearn)
     --------------------
     Date created: 01/22/17
-    Date modified: 01/22/17
+    Date modified: 01/29/17
 """
 
 import scipy
@@ -16,6 +17,9 @@ import time
 from sklearn.feature_extraction.text import CountVectorizer
 from scipy.sparse.csr import csr_matrix
 import sys
+from sklearn import linear_model
+from sklearn.metrics import confusion_matrix
+import numpy as np
 
 __author__='Emily Ahn'
 
@@ -56,14 +60,14 @@ def get_langid(lang):
     return ids[lang]
 
 if __name__=='__main__':
-
-    k = int(sys.argv[1])
     
     thesis_dir = '..'#'Desktop/thesis/emilythesis'
     with open(thesis_dir+'/traintestsplit/7lang.trainlist.sorted.20sec', 'r') as reader:
         trainlist = [line.replace('\n','') for line in reader.readlines()]
     with open(thesis_dir+'/traintestsplit/7lang.testlist.sorted.20sec', 'r') as reader:
         testlist = [line.replace('\n','') for line in reader.readlines()]
+        
+    langlist = ['AR','CZ','FR','HI','IN','KO','MA']
                 
     train_filelist, traincats = get_flist_labels(trainlist, '..')
     test_filelist, testcats = get_flist_labels(testlist, '..')
@@ -82,10 +86,20 @@ if __name__=='__main__':
     testpoints = td_matrix[-len(testcats):]
     
     # run knn classifier
-    predictions = knn(trainpoints, traincats, testpoints, k)
+    #k = int(sys.argv[1])
+    #predictions = knn(trainpoints, traincats, testpoints, k)
+    
+    # run logistic regresion clasifier
+    logreg = linear_model.LogisticRegression()
+    print 'open logreg'
+    logreg.fit_transform(trainpoints, traincats)
+    print 'fitted logreg'
+    predictions = logreg.predict(testpoints)
+    print 'predicted logreg'
     
     # write actual category, predict category, and text of test points, and compute accuracy
-    o = codecs.open('knn.'+str(k)+'.predictions', 'w', 'utf8')
+    #o = codecs.open('knn.'+str(k)+'.predictions', 'w', 'utf8')4
+    o = codecs.open('logreg.predictions', 'w', 'utf8')
     o.write('ACTUAL,PREDICTED,CORRECT?,TEXT\n')
     o.write('{AR:0,CZ:1,FR:2,HI:3,IN:4,KO:5,MA:6}\n')
  
@@ -103,11 +117,22 @@ if __name__=='__main__':
             o.write('WRONG,')
         o.write(test_filelist[i]+'\n')
 
-    print 'Stored predictions in knn.'+str(k)+'.predictions', 'for test points'
+    #print 'Stored predictions in knn.'+str(k)+'.predictions', 'for test points'
+    print 'Stored predictions in logreg.predictions', 'for test points'
     acc = numcorrect*100/len(testcats)
     print 'Accuracy: {0:.2f}%'.format(acc)
     
-    
+    # CONFUSION MATRIX
+    cm = confusion_matrix(testcats, predictions)
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    # display confusion stats by lang (TODO: visualize with matplotlib)                                 
+    print '*'*20
+    for ai, actual_lang in enumerate(langlist):
+        print actual_lang, 'confusion:'
+        for pi, predicted_lang in enumerate(langlist):
+            print '{0}: {1:.2f}%'.format(predicted_lang, cm_normalized[ai, pi]*100)
+        print '*'*20
+
     
     
     
